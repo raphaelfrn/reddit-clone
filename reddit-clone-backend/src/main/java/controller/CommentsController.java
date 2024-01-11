8ac5e6979bf2683abf1ed854865e9a0b77169bb5
Raphaël <raphfar@gmail.com>
2024-01-11 17:54:48 +0100

    updated controllers

---
+++ reddit-clone-backend/src/main/java/controller/CommentsController.java
@@ -0,0 +1,44 @@
+package controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.CommentsDto;
+import lombok.AllArgsConstructor;
+import service.CommentService;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping("/api/comments")
+@AllArgsConstructor
+public class CommentsController {
+    private final CommentService commentService;
+
+    @PostMapping
+    public ResponseEntity<Void> createComment(@RequestBody CommentsDto commentsDto) {
+        commentService.save(commentsDto);
+        return new ResponseEntity<>(CREATED);
+    }
+
+    @GetMapping(params = "postId")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForPost(@RequestParam Long postId) {
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForPost(postId));
+    }
+
+    @GetMapping(params = "userName")
+    public ResponseEntity<List<CommentsDto>> getAllCommentsForUser(@RequestParam String userName){
+        return ResponseEntity.status(OK)
+                .body(commentService.getAllCommentsForUser(userName));
+    }
+
+}
---
+++ reddit-clone-backend/src/main/java/controller/PostController.java
@@ -0,0 +1,54 @@
+package controller;
+
+import static org.springframework.http.ResponseEntity.status;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import dto.PostRequest;
+import dto.PostResponse;
+import lombok.AllArgsConstructor;
+import service.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
+@AllArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<Void> createPost(@RequestBody PostRequest postRequest) {
+        postService.save(postRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        return status(HttpStatus.OK).body(postService.getAllPosts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponse> getPost(@PathVariable Long id) {
+        return status(HttpStatus.OK).body(postService.getPost(id));
+    }
+
+    @GetMapping(params = "subredditId")
+    public ResponseEntity<List<PostResponse>> getPostsBySubreddit(@RequestParam Long subredditId) {
+        return status(HttpStatus.OK).body(postService.getPostsBySubreddit(subredditId));
+    }
+
+    @GetMapping(params = "username")
+    public ResponseEntity<List<PostResponse>> getPostsByUsername(@RequestParam String username) {
+        return status(HttpStatus.OK).body(postService.getPostsByUsername(username));
+    }
+}
