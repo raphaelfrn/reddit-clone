8bed5f66a6a10582d96ad23753b66654d4380641
Raphaël <raphfar@gmail.com>
2024-01-24 22:10:11 +0100

    working on tests

---
+++ reddit-clone-backend/src/test/java/com/raphelfrn/redditclone/repository/PostRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.raphelfrn.redditclone.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.raphelfrn.redditclone.BaseTest;
+
+import model.Post;
+import repository.PostRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PostRepositoryTest extends BaseTest {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    public void shouldSavePost() {
+        Post expectedPostObject = new Post(null, "First Post", "http://url.site", "Test",
+                0, null, Instant.now(), null);
+        Post actualPostObject = postRepository.save(expectedPostObject);
+        assertThat(actualPostObject).usingRecursiveComparison()
+                .ignoringFields("postId").isEqualTo(expectedPostObject);
+    }
+
+}
---
+++ reddit-clone-backend/src/test/java/com/raphelfrn/redditclone/repository/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.raphelfrn.redditclone.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.raphelfrn.redditclone.BaseTest;
+
+import model.User;
+import repository.UserRepository;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest extends BaseTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void shouldSavePost() {
+        User expectedUserObject = new User(123L, "test user", "secret password", "user@email.com", Instant.now(), true);
+        User actualUserObject = userRepository.save(expectedUserObject);
+        assertThat(actualUserObject).usingRecursiveComparison()
+                .ignoringFields("userId").isEqualTo(expectedUserObject);
+    }
+
+}
